03f9f8d664d9f0459577df17aae3f285acd44dd3
Yury Kalenkou <goldyukol12@gmail.com>
2022-02-25 01:16:33 +0300

    feat: add brackets logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  const arr = str.split("");
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  arr.forEach((el) => {
+    if (brackets[stack[stack.length - 1]] === el) {
+      return stack.pop(el);
+    } 
+    stack.push(el);
+  });
+
+  return stack.length === 0;
+};
